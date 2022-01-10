ff1f8fa860e99abb2d6d44144c76930886b16277
ferroye <yewang8864@gmail.com>
2022-01-10 13:36:53 -0500

    first commit

---
+++ src/main/java/com/worf/worf/WorfApplication.java
@@ -0,0 +1,13 @@
+package com.worf.worf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorfApplication.class, args);
+	}
+
+}
